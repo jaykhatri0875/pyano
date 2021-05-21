4016aaae3488d522b7d99a1efc7b64d804147e21
JAY KHATRI <khatri.jaykumar.875@ldce.ac.in>
2021-05-21 13:53:43 +0530

    src added

--- src/pyano.py
+++ src/pyano.py
@@ -1,6 +1,11 @@
-## demo of running mediapipe in local machine
-## taking video feed from mobile and procesing
+## this is piano made out of python script and you can play air piano at normal distance from computer \
+## Accoring to camera width you can increase keys and also in notes you can add diff tones to play
 
+##Script uses Google's opensource handtracking framework - Mediapipe
+
+
+
+##importing necessary libraries 
 import cv2
 import time
 import mediapipe as mp
@@ -8,9 +13,11 @@ import numpy as np
 import notes as n
 import math
 
+#distance function which calculates geometric ditance between two points is plane
 def dist(p1x,p2x,p1y,p2y):
     return math.sqrt((p2y-p1y)**2+(p2x-p1x)**2)*100
 
+#is-pressed is booleanfunction checks whether user has made gesture which can be considered as key press or not
 def isP(arr):
     tx,ty,dx,dy,px,py=arr
     dis1 = dist(tx,dx,ty,dy)
@@ -19,7 +26,7 @@ def isP(arr):
     if(dis1 < 3.4 and dis2 < 5 and dis3 < 4):
         return True
     return
-
+#drawing lines on user screen for key reference
 def draw_lines(image):
     cv2.rectangle(image,(0,60),(40,250),green,thickness=2)
     cv2.rectangle(image,(40,60),(80,250),green,thickness=2)
@@ -41,15 +48,20 @@ def draw_lines(image):
     
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
+
 cap = cv2.VideoCapture(0)
+
 image_width  = cap.get(3)   
 image_hight = cap.get(4)
+
 green = (0,255,0)
+
 x_pos,y_pos = None,None
-count = 0
-f_vec = []
-#'http://192.168.0.101:8080/video'
-print("started getting feed")
+count = 0 ## not required
+f_vec = [] ## feature vector
+
+print("started getting feed (press q to quit)")
+
 with mp_hands.Hands(
     min_detection_confidence = 0.5,
     min_tracking_confidence = 0.5) as hands:
@@ -60,12 +72,14 @@ with mp_hands.Hands(
             continue
         draw_lines(image)
         image.flags.writeable = True
-        image = cv2.cvtColor(cv2.flip(image,1), cv2.COLOR_RGB2BGR)
-        results = hands.process(image)
+        image=cv2.flip(image,1)
+        image1 = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        results = hands.process(image1)
         if results.multi_hand_landmarks:
           for hand_landmarks in results.multi_hand_landmarks:
             mp_drawing.draw_landmarks(
                 image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+            ## getting index fingers coordinates here
             x_pos_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x 
             y_pos_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y
             x_pos_dip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_DIP].x 
@@ -75,6 +89,8 @@ with mp_hands.Hands(
             f_vec = [x_pos_tip,y_pos_tip,x_pos_dip,y_pos_dip,x_pos_pip,y_pos_pip]
             x_pos = x_pos_tip * image_width
             y_pos = y_pos_tip * image_hight
+            ## to check whether or not index finger is in key region or not and is pressing over the region
+            ## if pressed it will play tone according  to the key region 
             if(x_pos>0 and x_pos<40 and y_pos > 60 and y_pos < 250 and count==0 and isP(f_vec)):
                 n.play_a3()
                 count=1
@@ -135,6 +151,8 @@ with mp_hands.Hands(
                 count = 0
 
         cv2.imshow('MediaPipe Hands', image)
-        if cv2.waitKey(5) & 0xFF == 27:
-          break
+        if cv2.waitKey(33)==ord('q'):
+            print('thank you for using pyano !')
+            break
     cap.release()
+    cv2.destroyAllWindows()
